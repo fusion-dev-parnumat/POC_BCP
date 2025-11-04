ce1ee24847398f9de1f0cf05d598438cfaea9664
fusion-dev-parnumat <parnumat@fusionsol.com>
2025-11-04 16:46:01 +0700

    re commit

--- nb_prod.Notebook/notebook-content.py
+++ nb_prod.Notebook/notebook-content.py
@@ -8,15 +8,12 @@
 # META   },
 # META   "dependencies": {
 # META     "lakehouse": {
-# META       "default_lakehouse": "03d30ad3-395f-469e-80f3-847358a4e5a0",
-# META       "default_lakehouse_name": "lh_000_poc",
+# META       "default_lakehouse": "a7c37b4e-4713-4858-84ac-da74f19d7f32",
+# META       "default_lakehouse_name": "lh_prod",
 # META       "default_lakehouse_workspace_id": "5234467e-652b-4189-9f32-78ab6b60ea1c",
 # META       "known_lakehouses": [
 # META         {
-# META           "id": "495ad4f2-6622-4a38-83d7-aff00b104206"
-# META         },
-# META         {
-# META           "id": "03d30ad3-395f-469e-80f3-847358a4e5a0"
+# META           "id": "a7c37b4e-4713-4858-84ac-da74f19d7f32"
 # META         }
 # META       ]
 # META     }
@@ -41,11 +38,15 @@ schema = StructType([
 data = [
     ("B", 45, 67, 5, 11.9, Decimal('9.5555')),
     ("B", 1, 5, 5, 5.9, Decimal('3.1111')),
+    ("B", 2, 20, 6, 2.5, Decimal('0.01')),
+    ("B", 2, 20, 6, 2.5, Decimal('0.01')),
+    ("B", 2, 20, 6, 2.5, Decimal('0.01')),
+    ("B", 2, 20, 6, 2.5, Decimal('0.01')),
     ("B", 2, 20, 6, 2.5, Decimal('0.01'))
 ]
         
 df = spark.createDataFrame(data, schema)
-df.write.mode('overwrite').save("abfss://POV_DEV@onelake.dfs.fabric.microsoft.com/lh_001_poc.Lakehouse/Tables/001_poc")
+df.write.mode('overwrite').save("abfss://POV_PROD@onelake.dfs.fabric.microsoft.com/lh_prod.Lakehouse/Tables/001_poc_prod")
 
 # METADATA ********************
 
