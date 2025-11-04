620205afad911f94454920fac16d445dc77beae7
fusion-dev-parnumat <parnumat@fusionsol.com>
2025-11-04 15:16:14 +0700

    dev first commit

---
+++ nb_poc.Notebook/notebook-content.py
@@ -0,0 +1,55 @@
+# Fabric notebook source
+
+# METADATA ********************
+
+# META {
+# META   "kernel_info": {
+# META     "name": "synapse_pyspark"
+# META   },
+# META   "dependencies": {
+# META     "lakehouse": {
+# META       "default_lakehouse": "eda0d7dd-a1a3-4b6f-b574-baf8d9e229e5",
+# META       "default_lakehouse_name": "lh_000_poc",
+# META       "default_lakehouse_workspace_id": "2fd3687f-104a-4ada-a52e-22a1720ecd6b",
+# META       "known_lakehouses": [
+# META         {
+# META           "id": "eda0d7dd-a1a3-4b6f-b574-baf8d9e229e5"
+# META         },
+# META         {
+# META           "id": "4bc16e0e-5095-443c-a0f7-0f6457e5f600"
+# META         }
+# META       ]
+# META     }
+# META   }
+# META }
+
+# CELL ********************
+
+from pyspark.sql.types import *
+from pyspark.sql.functions import *
+from decimal import Decimal
+
+schema = StructType([
+    StructField("s", StringType(), True),
+    StructField("i", IntegerType(), True),
+    StructField("l", LongType(), True),
+    StructField("sh", ShortType(), True),
+    StructField("d", DoubleType(), True),
+    StructField("dec", DecimalType(10, 2), True)
+])
+        
+data = [
+    ("B", 45, 67, 5, 11.9, Decimal('9.5555')),
+    ("B", 1, 5, 5, 5.9, Decimal('3.1111')),
+    ("B", 2, 20, 6, 2.5, Decimal('0.01'))
+]
+        
+df = spark.createDataFrame(data, schema)
+df.write.mode('overwrite').save("abfss://POV_DEV@onelake.dfs.fabric.microsoft.com/lh_001_poc.Lakehouse/Tables/001_poc")
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
